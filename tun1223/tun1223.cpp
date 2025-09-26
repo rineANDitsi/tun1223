5dc9059d1c12597a1169108e303e71346bf0e1c0
rine <kase.zp.ua@gmail.com>
2025-09-26 18:21:44 +0300

    How many day in years

--- tun1223/tun1223.cpp
+++ tun1223/tun1223.cpp
@@ -3,6 +3,12 @@ using namespace std;
 
 int main()
 {
-
+	cout << "Years is -  ";
+	int year;
+	cin >> year;
+	if (year % 10 != 0 && year % 4 == 0 || year % 400 == 0) {
+		cout << "Is leap year - 366 days";
+	}
+	else cout << "Is not leap year - 365 days";
 }
 
