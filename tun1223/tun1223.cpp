44005b25fe78dd45298a4a133b48b2dc8823170a
rine <kase.zp.ua@gmail.com>
2025-09-26 19:14:41 +0300

    Added color of text

--- tun1223/tun1223.cpp
+++ tun1223/tun1223.cpp
@@ -1,14 +1,40 @@
 ﻿#include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-	cout << "Years is -  ";
+	//1
+	/*cout << "Years is -  ";
 	int year;
 	cin >> year;
 	if (year % 10 != 0 && year % 4 == 0 || year % 400 == 0) {
 		cout << "Is leap year - 366 days";
 	}
 	else cout << "Is not leap year - 365 days";
+	*/
+	//2
+	string arr[]{ "\033[31m", "\033[32m", "\03333m", "\033[41m", "\033[42m", "\033[43m" };
+	int colorOfText, colorBack;
+	while (true) {
+		system("cls");
+		cout << "\n1. Red\n2. Green\n3. White\nText color is - ";
+		cin >> colorOfText;
+		if (colorOfText > 0 && colorOfText < 4) {
+			cout << "colorOfText: " << colorOfText;
+			while (true) {
+				system("cls");
+				cout << "\n1. Red\n2. Green\n3. Yellow\nBackground color is - ";
+				cin >> colorBack;
+				if (colorBack > 0 && colorBack < 4) {
+					colorBack += 3;
+					break;
+				}
+			}
+			break;
+		}
+	}
+	cout << arr[colorOfText-1] << arr[colorBack-1] << "\nText lalalal\033[0m";
 }
+
 
